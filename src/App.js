c4522bf2224b313f762c6f0e96d60ddc6e930b7d
Yuhei-shika <shikanono.1@gmail.com>
2025-06-05 12:16:01 +0900

    imagesの表示2

--- src/App.js
+++ src/App.js
@@ -19,13 +19,22 @@ function App() {
     <div className="container">
       <main>
         <div className="cards-container">
-          {characters.map((character) => (
+          {characters.map((character) => {
+            return (
             <div className="card" key={character.id}>
               <img src={character.images[0] != null ? character.images[0] : 'dummy.png'}
               alt="character" 
               className='card-image'></img>
+              <div className="card-content">
+                <h3 className="card-title">{character.name}</h3>
+                <p className="card-description">{character.debut != null ? character.debut.appearsIn : 'なし'}</p>
+                <div className="card-footer">
+                  <span className="affiliation">{character.personal != null ? character.personal.affiliation : 'なし'}</span>
+                </div>
+              </div>
             </div>
-          ))}
+          );
+          })}
         </div>  
       </main>
     </div>
