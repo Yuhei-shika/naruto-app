1411c84c0e8ce2a5d94399a8a45fee22e508c19a
Yuhei-shika <shikanono.1@gmail.com>
2025-06-05 11:58:11 +0900

    imagesの表示

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 function App() {
+  const [characters, setCharacters] = useState([]);
   useEffect(() => {
     fetchCharacters();
   }, []);
@@ -10,14 +11,25 @@ function App() {
     const apiUrl = 'http://localhost:80/character';
 
     const result = await axios.get(apiUrl);
+    setCharacters(result.data.characters);
     console.log(result);
   };
 
   return (
-    <div className="App">
-      <h1>Naruto App</h1>
+    <div className="container">
+      <main>
+        <div className="cards-container">
+          {characters.map((character) => (
+            <div className="card" key={character.id}>
+              <img src={character.images[0] != null ? character.images[0] : 'dummy.png'}
+              alt="character" 
+              className='card-image'></img>
+            </div>
+          ))}
+        </div>  
+      </main>
     </div>
   );
-}
+} 
 
 export default App;
